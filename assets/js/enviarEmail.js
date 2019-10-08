defa4cc101d7417f256912579c867c22af6f247e
FIAP <FIAP>
2019-10-08 20:00:10 -0300

    criacao do modal

--- assets/js/enviarEmail.js
+++ assets/js/enviarEmail.js
@@ -4,6 +4,23 @@ function enviaEmail() {
     const email = document.getElementById('email-contato').value;
     const mensagem = document.getElementById('mensagem-contato').value;
 
+    document.getElementById('modal').style.display = 'block'
+    document.getElementById('modal-alert').style.display = 'block'
+
+    if (nome != '' && assunto != '' && email != '' && mensagem != '') {
+        envioOk();
+    } else {
+        clearModal();
+        document.getElementById('message-error').innerText = 'Preencha todos os campos!';
+        document.getElementById('error-icon').style.display = 'block';
+    }
+    
+}
+
+function envioOk() {    
+    clearModal();
+    document.getElementById('load-icon').style.display = 'block'
+
     const msg = {
         from: "renato.sousa@assessoria.app",
         to: "renato.sousainfo@gmail.com",
@@ -28,8 +45,13 @@ function enviaEmail() {
         }
     ).then(data => {
         limpaCampos()
+        clearModal();
+        document.getElementById('success-error').innerText = 'E-mail enviado com sucesso!';
+        document.getElementById('success-icon').style.display = 'block';
     }).catch(err => {
-
+        clearModal();
+        document.getElementById('message-error').innerText = 'Ocorreu algum erro durante o envio, tente novamente!';
+        document.getElementById('error-icon').style.display = 'block';
     })
 }
 
@@ -38,4 +60,18 @@ function limpaCampos() {
     document.getElementById('assunto-contato').value = '';
     document.getElementById('email-contato').value = '';
     document.getElementById('mensagem-contato').value = '';
+}
+
+function clearModal() {
+    var displaysModal = document.getElementsByClassName('displays-modal');
+
+    for (var i = 0; i != displaysModal.length; ++i) {
+        displaysModal[i].style.display = 'none';
+    }
+}
+
+function fecharModal() {
+    limpaCampos()
+    document.getElementById('modal').style.display = 'none'
+    document.getElementById('modal-alert').style.display = 'none'
 }
